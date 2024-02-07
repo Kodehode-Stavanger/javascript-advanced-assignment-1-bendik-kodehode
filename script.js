81b8bd3c644a9a78d24adece42ae76f0907d4a96
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-02-07 22:24:59 +0100

    Added collision detection, freezing of all boxes when game over, removal of previous boxes when starting again

--- script.js
+++ script.js
@@ -1,85 +1,114 @@
-const box = document.getElementById("box");
+const playerBox = document.getElementById("box");
 const wrapper = document.getElementById("wrapper");
 const container = document.getElementById("container");
-const startBtn = document.getElementById("start-btn")
+const startBtn = document.getElementById("start-btn");
+let intervals = [];
 
 // Adjustable dimensions
-box.style.width = "30px";
-box.style.height = "30px";
+playerBox.style.width = "30px";
+playerBox.style.height = "30px";
 wrapper.style.width = "500px";
 wrapper.style.height = "800px";
 
-const boxHeight = parseInt(box.style.height)
-const boxWidth = parseInt(box.style.width)
 const wrapperHeight = wrapper.offsetHeight
 const wrapperWidth = wrapper.offsetWidth
 
 container.style.height = `${wrapperHeight / 4}px`;
 container.style.width = `${wrapperWidth}px`;
-const containerHeight = parseInt(container.style.height);
-const containerWidth = parseInt(container.style.width);
+const containerHeight = container.offsetHeight;
 
-box.style.top = `${containerHeight - boxHeight}px`;
-box.style.left = `${(wrapperWidth / 2) - (boxWidth / 2)}px`;
+const playerBoxHeight = playerBox.offsetHeight;
+const playerBoxWidth = playerBox.offsetWidth;
+playerBox.style.top = `${containerHeight - playerBoxHeight}px`;
+playerBox.style.left = `${(wrapperWidth / 2) - (playerBoxWidth / 2)}px`;
 
 const moveSpeed = 20;
 const fallSpeed = 5;
 const fallSpeedInterval = 20;
+const boxSpawnInterval = 5;
 
 startBtn.addEventListener("click", () => {
-    // startBtn.style.visibility = "hidden";
+    startBtn.style.visibility = "hidden";
+    removePreviousBoxes();
     spawnFallingBoxes();
 })
 
 window.addEventListener("keypress", (event) => {
-    const boxTop = parseInt(box.style.top);
-    const boxLeft = parseInt(box.style.left)
+    const playerBoxTop = playerBox.offsetTop;
+    const playerBoxLeft = playerBox.offsetLeft;
 
     if (event.key === "w" || event.key === "ArrowUp") {
-        if (boxTop < (0 + boxHeight)) box.style.top = "0px"
-        else box.style.top = `${boxTop - moveSpeed}px`
+        if (playerBoxTop < (0 + playerBoxHeight)) playerBox.style.top = "0px"
+        else playerBox.style.top = `${playerBoxTop - moveSpeed}px`
     }
     else if (event.key === "s" || event.key === "ArrowDown") {
-        if (boxTop > (containerHeight - boxHeight - moveSpeed)) box.style.top = `${containerHeight - boxHeight}px`
-        else box.style.top = `${boxTop + moveSpeed}px`
+        if (playerBoxTop > (containerHeight - playerBoxHeight - moveSpeed)) playerBox.style.top = `${containerHeight - playerBoxHeight}px`
+        else playerBox.style.top = `${playerBoxTop + moveSpeed}px`
     }
     else if (event.key === "a" || event.key === "ArrowLeft") {
-        if (boxLeft < (0 + boxWidth)) box.style.left = "0px"
-        else box.style.left = `${boxLeft - moveSpeed}px`
+        if (playerBoxLeft < (0 + playerBoxWidth)) playerBox.style.left = "0px"
+        else playerBox.style.left = `${playerBoxLeft - moveSpeed}px`
     }
     else if (event.key === "d" || event.key === "ArrowRight") {
-        if (boxLeft >= (wrapperWidth - boxWidth - moveSpeed)) box.style.left = `${containerWidth - boxWidth}px`
-        else box.style.left = `${boxLeft + moveSpeed}px`
+        if (playerBoxLeft >= (wrapperWidth - playerBoxWidth - moveSpeed)) playerBox.style.left = `${container.offsetWidth - playerBoxWidth}px`
+        else playerBox.style.left = `${playerBoxLeft + moveSpeed}px`
     }
 })
 
 container.addEventListener("click", (event) => {
-    box.style.top = `${event.offsetY - (boxHeight / 2)}px`;
-    box.style.left = `${event.offsetX - (boxWidth / 2)}px`;
+    playerBox.style.top = `${event.offsetY - (playerBoxHeight / 2)}px`;
+    playerBox.style.left = `${event.offsetX - (playerBoxWidth / 2)}px`;
 })
 
 function spawnFallingBoxes () {
     const fallingBox = document.createElement("div");
 
-    fallingBox.classList.add("fallingBox");
+    fallingBox.classList.add("falling-box");
     fallingBox.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`
     wrapper.append(fallingBox)
     
     fallingBox.style.top = `${0 - fallingBox.offsetHeight}px`
-    '`${wrapper.offsetTop}px`'
     fallingBox.style.left = `${Math.floor(Math.random() * (wrapper.offsetWidth - fallingBox.offsetWidth))}px`
 
     const interval = setInterval(() => {
         if (fallingBox.offsetTop < wrapper.offsetHeight) {
             fallingBox.style.top = `${fallingBox.offsetTop + fallSpeed}px`
+            if (checkCollision(fallingBox)) {
+                clearAllIntervals();
+                alert("Game over!");
+                startBtn.style.visibility = "visible";
+            }
         }
         else {
             clearInterval(interval);
             fallingBox.remove()
         }
-    }, fallSpeedInterval);  
+    }, fallSpeedInterval); 
+
+    intervals.push(interval)
 }
 
-function checkCollision () {
+function checkCollision (fallingBox) {
+    const checkTop = () => {
+        return (playerBox.offsetTop < (fallingBox.offsetTop + fallingBox.offsetHeight))
+    }
 
+    if (((playerBox.offsetLeft < (fallingBox.offsetLeft + fallingBox.offsetWidth)) && checkTop())
+        && 
+        (((playerBox.offsetLeft + playerBox.offsetWidth) > fallingBox.offsetLeft) && checkTop())){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function clearAllIntervals() {
+    intervals.forEach((interval) => clearInterval(interval))
+    intervals = [];
+}
+
+function removePreviousBoxes() {
+    let previousBoxes = document.querySelectorAll(".falling-box");
+    previousBoxes.forEach((box) => box.remove());
 }
