a25ba582b812017e5734986fba2df6fb1b59089e
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-02-08 13:24:30 +0100

    Added spawning interval for spawning several boxes, modified container position to relative and adjusted offsets to correspond

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-const playerBox = document.getElementById("box");
+const playerBox = document.getElementById("player-box");
 const wrapper = document.getElementById("wrapper");
 const container = document.getElementById("container");
 const startBtn = document.getElementById("start-btn");
 let intervals = [];
+let gameOver = false;
 
 // Adjustable dimensions
+const playAreaRatio = 4;
 playerBox.style.width = "30px";
 playerBox.style.height = "30px";
-wrapper.style.width = "500px";
-wrapper.style.height = "800px";
+wrapper.style.width = "600px";
+wrapper.style.height = "600px";
 
 const wrapperHeight = wrapper.offsetHeight
 const wrapperWidth = wrapper.offsetWidth
 
-container.style.height = `${wrapperHeight / 4}px`;
+container.style.height = `${wrapperHeight / playAreaRatio}px`;
 container.style.width = `${wrapperWidth}px`;
 const containerHeight = container.offsetHeight;
 
 const playerBoxHeight = playerBox.offsetHeight;
 const playerBoxWidth = playerBox.offsetWidth;
-playerBox.style.top = `${containerHeight - playerBoxHeight}px`;
+playerBox.style.top = `${wrapperHeight - playerBoxHeight}px`;
 playerBox.style.left = `${(wrapperWidth / 2) - (playerBoxWidth / 2)}px`;
 
-const moveSpeed = 20;
+const playerBoxStep = 20;
 const fallSpeed = 5;
 const fallSpeedInterval = 20;
-const boxSpawnInterval = 5;
+const boxSpawnRate = 500;
 
 startBtn.addEventListener("click", () => {
     startBtn.style.visibility = "hidden";
     removePreviousBoxes();
-    spawnFallingBoxes();
-})
-
-window.addEventListener("keypress", (event) => {
-    const playerBoxTop = playerBox.offsetTop;
-    const playerBoxLeft = playerBox.offsetLeft;
-
-    if (event.key === "w" || event.key === "ArrowUp") {
-        if (playerBoxTop < (0 + playerBoxHeight)) playerBox.style.top = "0px"
-        else playerBox.style.top = `${playerBoxTop - moveSpeed}px`
-    }
-    else if (event.key === "s" || event.key === "ArrowDown") {
-        if (playerBoxTop > (containerHeight - playerBoxHeight - moveSpeed)) playerBox.style.top = `${containerHeight - playerBoxHeight}px`
-        else playerBox.style.top = `${playerBoxTop + moveSpeed}px`
-    }
-    else if (event.key === "a" || event.key === "ArrowLeft") {
-        if (playerBoxLeft < (0 + playerBoxWidth)) playerBox.style.left = "0px"
-        else playerBox.style.left = `${playerBoxLeft - moveSpeed}px`
-    }
-    else if (event.key === "d" || event.key === "ArrowRight") {
-        if (playerBoxLeft >= (wrapperWidth - playerBoxWidth - moveSpeed)) playerBox.style.left = `${container.offsetWidth - playerBoxWidth}px`
-        else playerBox.style.left = `${playerBoxLeft + moveSpeed}px`
-    }
-})
+    const intervalID = setInterval(() => {
+        if (!(gameOver)) spawnFallingBoxes();
+        else clearInterval(intervalID);
+    }, boxSpawnRate)
+});
 
 container.addEventListener("click", (event) => {
-    playerBox.style.top = `${event.offsetY - (playerBoxHeight / 2)}px`;
+    playerBox.style.top = `${event.offsetY - (playerBoxHeight / 2) + (wrapperHeight - containerHeight)}px`;
     playerBox.style.left = `${event.offsetX - (playerBoxWidth / 2)}px`;
+})
+
+window.addEventListener("keydown", (event) => {
+    const playerBoxTop = playerBox.offsetTop;
+    const playerBoxLeft = playerBox.offsetLeft;
+    const difference = wrapperHeight - containerHeight;
+    console.log(parseInt(playerBox.style.height))
+
+    if (event.key === "w" || event.key === "ArrowUp") {
+        if (playerBoxTop < (difference + playerBoxHeight)) playerBox.style.top = `${difference}px`
+        else playerBox.style.top = `${playerBoxTop - playerBoxStep}px`
+    }
+    if (event.key === "s" || event.key === "ArrowDown") {
+        if (playerBoxTop > (wrapperHeight - playerBoxHeight - playerBoxStep)) playerBox.style.top = `${wrapperHeight - playerBoxHeight}px`
+        else playerBox.style.top = `${playerBoxTop + playerBoxStep}px`
+    }
+    if (event.key === "a" || event.key == "ArrowLeft") {
+        if (playerBoxLeft - playerBoxStep < 0) playerBox.style.left = "0px"
+        else playerBox.style.left = `${playerBoxLeft - playerBoxStep}px`
+    }
+    if (event.key === "d" || event.key === "ArrowRight") {
+        if (playerBoxLeft >= (wrapperWidth - playerBoxWidth - playerBoxStep)) playerBox.style.left = `${container.offsetWidth - playerBoxWidth}px`
+        else playerBox.style.left = `${playerBoxLeft + playerBoxStep}px`
+    }
 })
 
 function spawnFallingBoxes () {
@@ -70,28 +77,29 @@ function spawnFallingBoxes () {
     fallingBox.style.top = `${0 - fallingBox.offsetHeight}px`
     fallingBox.style.left = `${Math.floor(Math.random() * (wrapper.offsetWidth - fallingBox.offsetWidth))}px`
 
-    const interval = setInterval(() => {
+    const intervalID = setInterval(() => {
         if (fallingBox.offsetTop < wrapper.offsetHeight) {
             fallingBox.style.top = `${fallingBox.offsetTop + fallSpeed}px`
             if (checkCollision(fallingBox)) {
                 clearAllIntervals();
+                gameOver = true;
                 alert("Game over!");
                 startBtn.style.visibility = "visible";
             }
         }
         else {
-            clearInterval(interval);
+            clearInterval(intervalID);
             fallingBox.remove()
         }
     }, fallSpeedInterval); 
 
-    intervals.push(interval)
+    intervals.push(intervalID)
 }
 
 function checkCollision (fallingBox) {
     const checkTop = () => {
         return (playerBox.offsetTop < (fallingBox.offsetTop + fallingBox.offsetHeight))
-    }
+    };
 
     if (((playerBox.offsetLeft < (fallingBox.offsetLeft + fallingBox.offsetWidth)) && checkTop())
         && 
@@ -101,15 +109,15 @@ function checkCollision (fallingBox) {
     else {
         return false;
     }
-}
+};
 
 function clearAllIntervals() {
-    intervals.forEach((interval) => clearInterval(interval))
+    intervals.forEach((intervalID) => clearInterval(intervalID))
     intervals = [];
-}
+};
 
 function removePreviousBoxes() {
     let previousBoxes = document.querySelectorAll(".falling-box");
     previousBoxes.forEach((box) => box.remove());
     previousBoxes = []
-}
+};
